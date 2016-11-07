55be1b16a4fb90872432f346c4cd8d09bdf3b053
LucasNovaes2016 <lucasmsn200@gmail.com>
2016-11-07 11:31:26 -0300

    Add files via upload

---
+++ list1005.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+int main(){
+    std::vector<int> data{};
+    int x{};
+    // initialized to be empty
+    // Read integers one at a time.
+    while (std::cin >> x)
+    {
+
+    data.push_back(x);
+    if (data.size() == 3)
+    	break;
+    }
+
+    // Print the vector, one number per line.
+    int i(0); 
+    for (std::vector<int>::iterator it = data.begin() ; it != data.end(); ++it, ++i)
+    {
+        std::cout << data.at(i);
+        std::cout.fill (' ');
+        std::cout.width (10);
+        std::cout << data.at(i)*2;
+        std::cout.fill(' ');
+        std::cout.width (10);
+        std::cout << data.at(i)*data.at(i);
+        std::cout << "\n";
+    }
+}
